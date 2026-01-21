3be744fccec5495f72efc988b773328f7a243821
Sambit <113300326+Sambit1110@users.noreply.github.com>
2026-01-22 00:23:18 +0530

    Added dynamic notes using JavaScript and localStorage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Notes Sharing Platform Loaded");
+function addNote() {
+  let subject = document.getElementById("subject").value;
+  let title = document.getElementById("title").value;
+
+  if (subject === "" || title === "") {
+    alert("Please fill all fields");
+    return;
+  }
+
+  let notes = JSON.parse(localStorage.getItem("notes")) || [];
+
+  notes.push({ subject, title });
+
+  localStorage.setItem("notes", JSON.stringify(notes));
+
+  alert("Note added successfully!");
+
+  document.getElementById("subject").value = "";
+  document.getElementById("title").value = "";
+}
